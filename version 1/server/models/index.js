51268124b4092264f52dd6eb7e90599c8b22032e
Константин <konstantinzadoro@gmail.com>
2025-10-13 01:28:27 +0300

    connection db and create

---
+++ version 1/server/models/index.js
@@ -0,0 +1,72 @@
+const sequelize = require('./db');
+const { DataTypes } = require('sequelize');
+
+
+const User = sequelize.define('User', {
+    id_user: { 
+        type: DataTypes.INTEGER, 
+        primaryKey: true, 
+        autoIncrement: true 
+    },
+    login: { 
+        type: DataTypes.STRING(50), 
+        allowNull: false,
+        unique: true 
+    },
+    password: { 
+        type: DataTypes.STRING(250), 
+        allowNull: false 
+    },
+}, { 
+    tableName: 'users',  
+    timestamps: false 
+});
+
+const Promotion = sequelize.define('Promotion', {
+    id_promotion: {  
+        type: DataTypes.INTEGER, 
+        primaryKey: true, 
+        autoIncrement: true 
+    },
+    subdesc: { type: DataTypes.STRING(20) },
+    title: { type: DataTypes.STRING(50) }, 
+    desc: { type: DataTypes.TEXT }, 
+    price: { type: DataTypes.INTEGER },
+    sale: { type: DataTypes.INTEGER },
+    link: { type: DataTypes.STRING(50) },
+    time: { type: DataTypes.DATE },
+    image: { type: DataTypes.STRING(100) }, 
+}, { 
+    tableName: 'promotions', 
+    timestamps: false 
+});
+
+const Products = sequelize.define('Products', {
+    id_products: { 
+        type: DataTypes.INTEGER, 
+        primaryKey: true, 
+        autoIncrement: true 
+    },
+    title: DataTypes.STRING(30),
+    price: DataTypes.INTEGER,
+    size: DataTypes.ARRAY,
+    reviews: DataTypes.INTEGER,
+    desc: DataTypes.STRING(200),
+    stars:DataTypes.INTEGER,
+    tags:DataTypes.ARRAY,
+    category: DataTypes.STRING(30),
+    
+}, { 
+    tableName: 'products', 
+    timestamps: false 
+});
+
+
+
+
+module.exports = {
+    sequelize,
+    User,
+    Promotion,
+    Products
+};
