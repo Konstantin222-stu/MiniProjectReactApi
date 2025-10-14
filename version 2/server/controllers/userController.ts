9269693492b03b00a301e5fdbcb93414b53c297b
Константин <konstantinzadoro@gmail.com>
2025-10-14 19:25:34 +0300

    typed controller user

---
+++ version 2/server/controllers/userController.ts
@@ -0,0 +1,86 @@
+import { User } from "../models"
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import ApiError from "../error"
+import {Request, Response, NextFunction} from 'express'
+import {AuthRequest} from '../types/middleware'
+import { LoginRequestBody } from "../types/users"
+
+class UserController {
+
+  async checkUser(req: AuthRequest, res: Response, next: NextFunction): Promise<Response| void> {
+    try {
+      
+      if (!req.user?.id_user) { 
+        return next(ApiError.unauthorized('Токен не содержит ID пользователя'));
+      }
+  
+      const user = await User.findByPk(req.user.id_user, {
+        attributes: ['id_user', 'login']
+      });
+  
+      if (!user) {
+        return next(ApiError.notFound('Пользователь не найден в базе'));
+      }
+
+      const newToken = jwt.sign(
+        { id: user.id_user}, 
+        process.env.SECRET_KEY!,
+        { expiresIn: '24h' }
+      );
+  
+      return res.json({ 
+        token: newToken,
+        user: {
+          id: user.id_user, 
+          login: user.login,
+        }
+      });
+    } catch (err: unknown) {
+        if(err instanceof Error){
+          next(ApiError.badRequest(err.message))
+        }
+        else{
+          next(ApiError.internal('Неизвестная ошибка'))
+        }
+    }
+}
+
+
+  async login(req: Request, res:Response, next: NextFunction): Promise<Response | void> {
+    try {
+      const { login, password }:LoginRequestBody = req.body;
+
+      if (!login || !password) {
+        return next(ApiError.badRequest('Логин и пароль обязательны'));
+      }
+
+      const user = await User.findOne({ where: { login } });
+      if (!user) {
+        return next(ApiError.badRequest('Пользователь не найден'));
+      }
+
+      const comparePassword = await bcrypt.compare(password, user.password);
+      if (!comparePassword) {
+        return next(ApiError.badRequest('Неверный пароль'));
+      }
+
+      const token = jwt.sign(
+        { id: user.id_user },
+        process.env.SECRET_KEY!,
+        { expiresIn: '24h' }
+      );
+
+      return res.json({ token });
+    } catch (err: unknown) {
+      if(err instanceof Error){
+        next(ApiError.badRequest(err.message))
+      }
+      else{
+        next(ApiError.internal('Неизвестная ошибка'));
+      }
+    }
+  }
+}
+
+export default new UserController();
--- version 2/server/index.ts
+++ version 2/server/index.ts
@@ -1,41 +1,54 @@
-require('dotenv').config();
-const express = require('express')
-const application = express()
-const fileUpload = require('express-fileupload')
-const path = require('path')
-const cors = require('cors')
+import dotenv from 'dotenv'
+dotenv.config();
 
-const sequelize = require('./models/db')
-const modules = require('./models')
-const router = require('./routers')
+import express, {Express,Request,Response,NextFunction} from 'express'
+import fileUpload from 'express-fileupload'
+import path from 'path'
+import cors,{CorsOptions} from 'cors'
+import sequelize from './models/db'
+import * as modules from './models'
+import router from './routers'
+import ApiError from './error';
 
+const application: Express = express()
 
-application.use(cors({
+const CorsOptions:CorsOptions ={
     credentials: true,
     origin: "http://localhost:5173"
-}));
-
+}
+application.use(cors(CorsOptions))
 
 application.use(express.json())
 application.use(express.static(path.resolve(__dirname, 'uploads')))
-application.use(fileUpload({}))
-
+application.use(fileUpload({
+    limits : { fileSize: 50 * 1024 * 1024}
+}))
 
 application.use('/server', router)
 
+application.use((err: Error, req: Request, res: Response, next: NextFunction): Response => {
+    console.error('❌ Необработанная ошибка:', err);
+    
+    if (err instanceof ApiError) {
+        return res.status(err.status).json(err.toJSON());
+    }
+    
+    const apiError = ApiError.internal('Внутренняя ошибка сервера');
+    return res.status(apiError.status).json(apiError.toJSON());
+});
 
 
-const start = async ()=>{
+
+const start = async ():Promise<void>=>{
     try{
         await sequelize.authenticate()
         await sequelize.sync()
-        application.listen(3001, ()=>{
+        application.listen(3001, ():void=>{
             console.log('Робит')
         })
     }
-    catch(e){
+    catch(e:unknown){
         console.log(e);
-        
     }
 }
 
