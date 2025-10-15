a717d04983e18c8353fffd1c7128296621d4f858
Константин <konstantinzadoro@gmail.com>
2025-10-15 20:12:04 +0300

    typed main, App

--- version 2/client/src/App.tsx
+++ version 2/client/src/App.tsx
@@ -1,11 +1,16 @@
-import { useState } from 'react'
+import type React from 'react'
+import AppRouter from './router/AppRouter'
+import Footer from './section/footer/Footer'
+import Header from './section/header/Header'
 
-function App() {
-
+function App(): React.JSX.Element {
   return (
     <>
+      <Header/>
+      <AppRouter/>
+      <Footer/>
     </>
-  )
+  );
 }
 
 export default App
--- version 2/client/src/main.tsx
+++ version 2/client/src/main.tsx
@@ -1,9 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import App from './App.jsx'
+import './styles.css'
+import { BrowserRouter } from 'react-router-dom'
+import { AuthProvider } from './context/AuthorizationContext.jsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </StrictMode>
 )
